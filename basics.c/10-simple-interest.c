f6245cf7b5bb27bd8cf1697b8492f62f1393681e
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-02-02 22:09:04 +0530

    some que on variable

---
+++ basics.c/10-simple-interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    float p,r,t,SI;
+    p=100;
+    r=10;
+    t=2;
+    SI= (p*t*r)/100;
+    printf("the simple interest is: %f",SI);
+    return 0;
+}
---
+++ basics.c/8calculating-%.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    float x1 =100; //biology
+    float x2 =77;  //physicas
+    float x3 =94;  //maths 
+    float x4 =90;  //chemistry
+    float x5 =86;  //english
+    float m = (x1+x2+x3+x4+x5)/5;
+    printf("the percentage of the above 5 marks are %f",m); 
+
+    return 0;
+}
