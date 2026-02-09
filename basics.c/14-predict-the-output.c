257cce84e69a190eaa269f07a95b4b7cc79a953c
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-02-09 14:58:42 +0530

    data type

--- basics.c/14-predict-the-output.c
+++ basics.c/14-predict-the-output.c
@@ -7,3 +7,14 @@ int main(){
     printf(" p=%d\n q=%d",p,q);
     return 0;
 }
+
+
+//x=4+2%-8    evaluate value of x
+/*d=2/7.0 what will be the value of d if d is a float after the operation
+a=0
+b=0.2857
+c=cant be determined
+d=none of the above
+
+ANS= NONE OF THE ABOVE
+not b bcoz float gives value till 6 decimals and b is till 4 decimal..*/
---
+++ basics.c/20-char-datatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    char ch = 'G';
+    printf("%d",ch);  //if we use %d then ascii value of G will be printed.
+    return 0;
+}
+
+/*              ASCII VALUES
+
+A='65'...........Z='90'
+a='97'...........z='122'
+#='35'.... @='64'.....*='42'.....&='38'.....%='37'.....$='36'
+!='33'.....+='43'.....-='45'......_='95'....../='47'
+*/
+ 
