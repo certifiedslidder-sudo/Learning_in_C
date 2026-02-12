93aa08c8b532d423dd63cc56977adeb4af737f40
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-02-12 22:42:12 +0530

    if-else

--- if-else-in c/16-greatest-0f3-by-nested-if-else.c
+++ if-else-in c/16-greatest-0f3-by-nested-if-else.c
@@ -1,27 +1,30 @@
-#include<stdio.h>
-int main(){
-        int x,y,z;
+#include <stdio.h>
+int main()
+{
+    int x, y, z;
     printf("enter value of x");
     scanf("%d", &x);
     printf("enter value of y");
     scanf("%d", &y);
     printf("enter value of z");
     scanf("%d", &z);
-    if(x>y){  //b is out of race
-        if(x>z)
-            printf(" %d is the greatest",x);
-        
-        else{   //x<z
-            printf("%d is greatest ",z);
-        }
+    if (x > y)
+    { // b is out of race
+        if (x > z)
+            printf(" %d is the greatest", x);
 
-    }   
-        else{  // y>x     -> x ab sbse bada toh nhi h   
-            if(y>z)                         ///y>x
-              printf("%d is the greatest", y);
-            else   //z>y
-                print("% is greatest",z)  
+        else
+        { // x<z   -> y<x<z
+            printf("%d is greatest ", z);
         }
+    }
+    else
+    {              // y>x     -> x ab sbse bada toh nhi h
+        if (y > z) /// y>x
+            printf("%d is the greatest", y);
+        else // z>y
+            printf("%d is greatest", z);
+    }
 
     return 0;
 }
---
+++ if-else-in c/19-grade.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter percentage:");
+    scanf("%d", &n);
+    // if(n>80){
+    //     printf("A grade");
+    // }
+    // else if(n>60){
+    //     printf("B grade");
+    // }
+    // else if(n>40){
+    //     printf("C grade");
+    // }
+    // else{
+    //     printf("D grade");
+    // }
+    if (n > 80)
+    {
+        printf("A grade\n");
+    }
+    else
+    {
+        if (n > 60)
+        {
+            printf("B grade\n");
+        }
+        else
+        {
+            if (n > 40)
+            {
+                printf("C grade\n");
+            }
+            else
+            {
+                printf("D grade\n");
+            }
+        }
+    }
+    return 0;
+}
