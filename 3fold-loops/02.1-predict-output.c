24e6920482b6997f751d35aeafbb5a1faec0deed
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-06-15 23:28:42 +0530

    Que-on-loop

---
+++ 3fold-loops/02.1-predict-output.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int j;
+     //printf("\n%d ", j );   //garabge value
+    while(j<= 10)
+    {
+    printf("\n%d ", j );   loop ke andr jaega hi nhi
+        j = j+1;
+    }
+    return 0;
+}
