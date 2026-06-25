87a557b91e7696c228eb974c1399c1c10fa806c7
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-06-25 22:20:30 +0530

    func.

---
+++ 5-functions and pointer/7-permutation-andh-combination.c
@@ -0,0 +1,28 @@
+/*
+print nCr using functions
+*/
+#include<stdio.h>
+int factorial(int x){
+    int fact=1;
+    for(int i=2;i<=x;i++){
+        fact =fact*i;
+    }
+    return fact;
+}
+int main(){
+//     int n;
+//     printf("enter n:");
+//     scanf("%d", &n);
+//     int r;
+//     printf("enter r:");
+//     scanf("%d",&r);
+//     int nfact = factorial(n);  //n!
+//     int rfact = factorial(r);  //r!
+//     int nrfact = factorial(n-r);  //(n-r)!
+
+// int ncr = nfact/(rfact*nrfact);
+// printf("%d", ncr);
+int a = factorial(4);
+printf("%d",a);
+    return 0;
+}
