944576c941260032b3627541938d09ed8ba0ac8e
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-07-04 23:44:32 +0530

    basic=printing of 2d array

---
+++ 8-2D-array/3-StoreRollNumber.c
@@ -0,0 +1,23 @@
+/*WAP  to store roll number and marks obtained by 4 students side by side in a matrix.*/
+
+#include<stdio.h>
+int main(){
+    int r;
+    printf("enter the no. of rows");
+    scanf("%d",&r);
+    int arr[r][3] ;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&arr[i][j]);
+        }
+      
+}
+    printf("\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",arr[i][j]);
+        }
+      printf("\n");
+}
+    return 0;
+}
